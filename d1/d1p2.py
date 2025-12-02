4bc3dd6f92867f203e4bcfcd874c6cdbe682f81c
Wassim <wassimhachemi8@gmail.com>
2025-12-02 22:54:52 +0100

    p2 correctioin test

--- d1/d1p2.py
+++ d1/d1p2.py
@@ -10,13 +10,14 @@ for comb in combs:
     if comb[0] == "L":
         movement = int(comb[1:])
         hits += movement // 100
+        if current_dial > 0 and movement % 100 > current_dial:
+            hits += 1
         current_dial = (current_dial - movement) % 100
     else:
         movement = int(comb[1:])
         hits += movement // 100
+        if current_dial + (movement % 100) >= 100:
+            hits += 1
         current_dial = (current_dial + movement) % 100
-    
-    if current_dial == 0:
-        hits += 1
 
 print(hits)
--- d1/test.py
+++
@@ -1,6 +0,0 @@
-comb= -1055
-
-hits= int(comb/100)
-print(hits)
-current_dial = comb-(100*hits)
-print(current_dial)
