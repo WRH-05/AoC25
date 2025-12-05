bf3c3bf60f3cd4f91fa4eef0a67fbc762d6066f7
Wassim <wassimhachemi8@gmail.com>
2025-12-05 23:23:29 +0100

    my humble shitty code that's too low for the answer

--- d5/d5p1.py
+++ d5/d5p1.py
@@ -1,9 +1,45 @@
-
+id_ranges = []
+available_ids = []
+fresh_id_ranges = []
+s = []
+fresh_id_range_len = 0
+fresh_ids = 0
+rotten_ids = 0
 
 with open("d5/d5p1.txt", "r") as file:
-    for line in file:
-        id_ranges = list(line.strip().split("-"))
-        print(id_ranges)
-        if id_ranges == []:
+    lines = file.readlines() 
+
+for line in lines:
+    line = line.strip()
+    if line == "": 
+        break
+    if "-" in line:
+        id_ranges.append(list(line.split("-")))
+
+start_index = len(id_ranges) + 1  
+for i in range(start_index, len(lines)):
+    line = lines[i].strip()
+    if line == "":
+        continue
+    if "-" not in line: 
+        available_ids.append(line)
+
+for i in available_ids:
+    is_fresh = False
+    for j in id_ranges:
+        if int(j[0]) < int(i) < int(j[1]):
+            is_fresh = True
+            fresh_ids += 1
+            fresh_id_ranges.append(range(int(j[0]), int(j[1])))
+            fresh_id_range_len += len(fresh_id_ranges)
+            fresh_id_ranges.clear
             break
         
+    if not is_fresh:
+            rotten_ids +=1
+
+
+
+print(f"fresh ids:{fresh_ids}")
+print(f"rotten ids:{rotten_ids}")
+print(f"fresh id length:{fresh_id_range_len}")
